345b16985b1cf723477c4e381a6309c7920f03a5
piandas <santiagobustosp@gmail.com>
2023-08-26 22:21:07 -0500

    Prueba github y pimeros pasos

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,23 @@
-#Primer archivo
-#papi y ahora qué D:
+#### Cargar paquetes siempre al inicio
+from platform import python_version ## versión de python
+import pandas as pd ### para manejo de datos
+import sqlite3 as sql #### para bases de datos sql
+import sys ## saber ruta de la que carga paquetes
+
+python_version() ### verificar version de python
+
+####################################################################################################################
+########################  1. Comprender y limpiar datos ##################################################################
+####################################################################################################################
+########   Verificar lectura correcta de los datos
+########   Verificar Datos faltantes (eliminar variables si es necesario) (la imputación está la parte de modelado)
+########   Tipos de variables (categoricas/numéricas/fechas)
+########   Niveles en categorícas 
+########   Observaciones por categoría
+########   Datos atípicos en numéricas
+
+
+### Cargar tablas de datos desde github ###
+#action=("data//tbl_Action.csv")  
+#employees=("data//tbl_Employee.csv")  
+#performance=("data//tbl_Perf.csv")   
