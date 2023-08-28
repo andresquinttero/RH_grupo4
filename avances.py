b766aa7012cf13bec4b42c1761c12802e5a5c04c
piandas <santiagobustosp@gmail.com>
2023-08-27 23:42:55 -0500

    Se crrigieron las comas en 2 df

--- avances.py
+++ avances.py
@@ -1,35 +1,40 @@
 import pandas as pd ### para manejo de datos
 import sqlite3 as sql #### para bases de datos sql
-import sys ## saber ruta de la que carga paquetes
 
 ####################################################################################################################
 ########################  1. Comprender y limpiar datos ##################################################################
 ####################################################################################################################
 
+# Cargar las bases de datos en DataFrames
+df_employee= pd.read_csv('databases/employee_survey_data.csv')
+df_manager = pd.read_csv('databases/manager_survey_data.csv')
+# A estos 2 dataframes se cambio el delimitador ';' por ','
+df_general = pd.read_csv('databases/general_data.csv')
+df_retirement = pd.read_csv('databases/retirement_info.csv')
+
 # Cargar el diccionario de datos desde el archivo Excel para entender los campos de las bases de datos
-data_dictionary_path = 'databases/data_dictionary.xlsx'
-data_dictionary_df = pd.read_excel(data_dictionary_path)
-
+df_data_dictionary = pd.read_excel('databases/data_dictionary.xlsx')
 # Mostrar el diccionario de datos
-data_dictionary_df.head(len(data_dictionary_df))
+df_data_dictionary.head(len(df_data_dictionary))
 
-# Rutas de los archivos CSV
-employee_survey_data_path = 'databases/employee_survey_data.csv'
-general_data_path = 'databases/general_data.csv'
-in_time_path = 'databases/in_time.csv'
-manager_survey_data_path = 'databases/manager_survey_data.csv'
-out_time_path = 'databases/out_time.csv'
-retirement_info_path = 'databases/retirement_info.csv'
+###### Verificar lectura correcta de los datos
+df_employee.sort_values(by=['EmployeeID'],ascending=1).head(5)
+df_general.sort_values(by=['EmployeeID'],ascending=0).head(5)
+df_manager.sort_values(by=['EmployeeID'],ascending=0).head(5)
+df_retirement.sort_values(by=['EmployeeID'],ascending=0).head(5)
 
-# Cargar los archivos CSV en DataFrames de Python
-employee_survey_data_df = pd.read_csv(employee_survey_data_path)
-general_data_df = pd.read_csv(general_data_path)
-in_time_df = pd.read_csv(in_time_path)
-manager_survey_data_df = pd.read_csv(manager_survey_data_path)
-out_time_df = pd.read_csv(out_time_path)
-retirement_info_df = pd.read_csv(retirement_info_path)
+conn = sql.connect(':memory:')
 
+# Insertar DataFrames en la base de datos SQLite
+employee_survey_data.to_sql('employee_survey_data', conn, index=False)
+general_data.to_sql('general_data', conn, index=False)
+in_time.to_sql('in_time', conn, index=False)
+manager_survey_data.to_sql('manager_survey_data', conn, index=False)
+out_time.to_sql('out_time', conn, index=False)
+retirement_info.to_sql('retirement_info', conn, index=False)
 
+# Cerrar la conexión
+conn.close()
 ########   Verificar lectura correcta de los datos
 ########   Verificar Datos faltantes (eliminar variables si es necesario) (la imputación está la parte de modelado)
 ########   Tipos de variables (categoricas/numéricas/fechas)
