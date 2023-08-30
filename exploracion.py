30a4c3ef86105dd05f001a1a76993ff1416e7ce5
andresquinttero <andresquinttero@gmail.com>
2023-08-30 18:49:23 -0500

    QOL y código nuevo relevante

--- exploracion.py
+++ exploracion.py
@@ -33,7 +33,9 @@ plt.ylabel('Frecuencia')
 plt.show()
 
 # Convertir 'JobSatisfaction' a tipo int
-df['JobSatisfaction'] = df['JobSatisfaction'].astype(int, errors='ignore')
+df['JobSatisfaction'].dtype
+df['JobSatisfaction'] = df['JobSatisfaction'].astype(float).astype(int)
+df['JobSatisfaction'].unique()
 
 # Visualización de la relación entre la satisfacción laboral y la retención del empleado
 sns.boxplot(x='Attrition', y='JobSatisfaction', data=df)
@@ -97,12 +99,21 @@ attrition_correlation
 
 ##### ANALISIS #######
 #Correlaciones Positivas:
-#NumCompaniesWorked (0.042) y PercentSalaryHike (0.033) tienen correlaciones positivas bajas con Attrition. Esto sugiere que los empleados que han trabajado en más empresas o que han tenido aumentos de salario más significativos son un poco más propensos a abandonar la empresa.
+# NumCompaniesWorked (0.042) y PercentSalaryHike (0.033) tienen correlaciones positivas muy bajas con Attrition.
+# Esto sugiere que los empleados que han trabajado en más empresas o que han tenido 
+# aumentos de salario más significativos son un poco más propensos a abandonar la empresa. 
+# Sin embargo son correalaciones muy débiles que sugieren que no hay una relación significativa con Attrition
+
 #Correlaciones Negativas:
-#TotalWorkingYears (-0.171), Age (-0.159), y YearsWithCurrManager (-0.156) tienen correlaciones negativas moderadas. Esto podría indicar que los empleados más antiguos, de mayor edad, o con más tiempo con su gerente actual son menos propensos a abandonar la empresa.
-#Variables como MonthlyIncome (-0.031), YearsSinceLastPromotion (-0.033), y TrainingTimesLastYear (-0.049) también muestran una correlación negativa, aunque más baja.
+# TotalWorkingYears (-0.171), Age (-0.159), y YearsWithCurrManager (-0.156) tienen correlaciones negativas moderadas. 
+# Esto podría indicar que los empleados más antiguos, de mayor edad, o con más tiempo con su gerente actual
+# son menos propensos a abandonar la empresa.
+# Variables como MonthlyIncome (-0.031), YearsSinceLastPromotion (-0.033), y TrainingTimesLastYear (-0.049) 
+# también muestran una correlación negativa, aunque más baja.
 #Correlaciones Cercanas a Cero:
-#Variables como EmployeeID, StockOptionLevel, DistanceFromHome, y JobLevel tienen correlaciones muy cercanas a cero, lo que sugiere que probablemente no tengan un impacto significativo en la rotación de empleados.
+
+# Variables como EmployeeID, StockOptionLevel, DistanceFromHome, y JobLevel tienen correlaciones muy cercanas
+# a cero, lo que sugiere que probablemente no tengan un impacto significativo en la rotación de empleados.
 
 
 # Eliminamos las variables no numéricas y los NA para el modelo
@@ -119,8 +130,8 @@ importances = model.feature_importances_
 feature_names = X.columns
 
 feature_importance_df = pd.DataFrame({"Feature": feature_names, "Importance": importances})
-print("Importancia de las variables según el árbol de decisión:")
-print(feature_importance_df.sort_values(by="Importance", ascending=False))
+
+print("Importancia de las variables según el árbol de decisión:\n", feature_importance_df.sort_values(by="Importance", ascending=False))
 
 
 ### Analisis adicionales para attrition
@@ -146,11 +157,16 @@ plt.xlabel('YearsWithCurrManager')
 plt.ylabel('Frecuencia')
 plt.show()
 
-# Relación entre "TotalWorkingYears" y "Attrition"
+#------------------------------------------------------------------------
+
+# Según el arbol de decisión, estas son las 4 variables más importantes respecto a la objetivo, 
+# Con estos gráficos podemos ver la relación directa con el abandono de los empleados de la empresa.
+
+# Relación entre la variable objetivo Attrition y TotalWorkingYears
 sns.boxplot(x='Attrition', y='TotalWorkingYears', data=df)
-plt.title('Relación entre TotalWorkingYears y Attrition')
-plt.xlabel('Attrition')
-plt.ylabel('TotalWorkingYears')
+plt.title('Relación entre la variable objetivo y TotalWorkingYears')
+plt.xlabel('Abandono')
+plt.ylabel('Total de años trabajados')
 plt.show()
 
 # Relación entre "Age" y "Attrition"
@@ -159,6 +175,22 @@ plt.title('Relación entre Age y Attrition')
 plt.xlabel('Attrition')
 plt.ylabel('Age')
 plt.show()
+
+# Relación entre "NumCompaniesWorked" y "Attrition"
+sns.boxplot(x='Attrition', y='NumCompaniesWorked', data=df)
+plt.title('Relación entre número de empresas trabajadas y Attrition')
+plt.xlabel('Attrition')
+plt.ylabel('NumCompaniesWorked')
+plt.show()
+
+# Relación entre "MonthlyIncome" y "Attrition"
+sns.boxplot(x='Attrition', y='MonthlyIncome', data=df)
+plt.title('Relación entre el salario mensual y Attrition')
+plt.xlabel('Attrition')
+plt.ylabel('MonthlyIncome')
+plt.show()
+
+#------------------------------------------------------------------------
 
 # Relación entre "YearsWithCurrManager" y "Attrition"
 sns.boxplot(x='Attrition', y='YearsWithCurrManager', data=df)
@@ -181,21 +213,29 @@ plt.xlabel('Attrition')
 plt.ylabel('EnvironmentSatisfaction')
 plt.show()
 
-##### Analisis final
-#Mayor edad y más años de trabajo disminuyen la probabilidad de abandono: Las variables Age y TotalWorkingYears 
-#tienen una correlación negativa moderada con Attrition, lo que sugiere que los empleados más jóvenes y con menos experiencia son más propensos a abandonar la empresa.
+##### Análisis final
 
-#Gestión y tiempo en la empresa son clave: YearsWithCurrManager tiene una correlación negativa de -0.156 con 
-# Attrition. Esto puede implicar que una buena relación con el gerente actual o simplemente el tiempo pasado con el mismo gerente puede llevar a una menor tasa de rotación.
+# Mayor edad y más años de trabajo disminuyen la probabilidad de abandono: Las variables Age y TotalWorkingYears 
+# tienen una correlación negativa moderada con Attrition, también fueron las variables más importantes según 
+# nuestro arbol de decisión, lo cuál sugiere que los empleados más jóvenes y con menos experiencia son más
+# propensos a abandonar la empresa.
 
-#Satisfacción laboral y ambiental son importantes: Las variables JobSatisfaction y EnvironmentSatisfaction 
-# tienen correlaciones negativas de -0.104 y -0.101, respectivamente. Esto indica que cuanto mayor es la satisfacción laboral y ambiental, menor es la probabilidad de que un empleado abandone la empresa.
+# La gestión y el tiempo en la empresa son clave: YearsWithCurrManager tiene una correlación negativa de -0.156 
+# con Attrition. Esto puede implicar que una buena relación con el gerente actual o simplemente el tiempo 
+# pasado con el mismo gerente puede llevar a una menor tasa de rotación.
 
-#Cambios frecuentes de empresa podrían ser una señal de alerta: NumCompaniesWorked tiene una correlación 
-# positiva de 0.041 con Attrition. Aunque no es una correlación fuerte, podría ser un indicador de que aquellos empleados que han cambiado de empresa con más frecuencia en el pasado son más propensos a abandonar.
+# Satisfacción laboral y ambiental son importantes: Las variables JobSatisfaction y EnvironmentSatisfaction 
+# tienen correlaciones negativas de -0.104 y -0.101, respectivamente. Esto indica que cuanto mayor es la 
+# satisfacción laboral y ambiental, menor es la probabilidad de que un empleado abandone la empresa.
 
-#Aumento salarial no garantiza retención: Sorprendentemente, PercentSalaryHike tiene una correlación 
-# positiva baja (0.032) con Attrition, lo que sugiere que un aumento salarial no necesariamente garantiza la retención del empleado.
+# Cambios frecuentes de empresa podrían ser una señal de alerta: NumCompaniesWorked tiene una correlación 
+# positiva de 0.041 con Attrition. Aunque no es una correlación fuerte, podría ser un indicador de que aquellos 
+# empleados que han cambiado de empresa con más frecuencia en el pasado son más propensos a abandonar.
 
-#Nivel educativo y nivel laboral no son factores críticos: Las variables como Education y JobLevel tienen 
-# correlaciones muy cercanas a cero con Attrition, lo que sugiere que no son factores críticos en la decisión de un empleado de abandonar o quedarse en la empresa.
+# Aumento salarial no garantiza retención: Sorprendentemente, PercentSalaryHike tiene una correlación 
+# positiva baja (0.032) con Attrition, lo que sugiere que un aumento salarial no necesariamente garantiza
+# la retención del empleado.
+
+# Nivel educativo y nivel laboral no son factores críticos: Las variables como Education y JobLevel tienen 
+# correlaciones muy cercanas a cero con Attrition, lo que sugiere que no son factores críticos en la
+# decisión de un empleado de abandonar o quedarse en la empresa.
--- preprocesamiento.py
+++ preprocesamiento.py
@@ -20,9 +20,9 @@ df_data_dictionary.head(len(df_data_dictionary))
 
 ###### Verificar lectura correcta de los datos
 df_employee.sort_values(by=['EmployeeID'],ascending=1).head(5)
-df_general.sort_values(by=['EmployeeID'],ascending=0).head(5)
-df_manager.sort_values(by=['EmployeeID'],ascending=0).head(5)
-df_retirement.sort_values(by=['EmployeeID'],ascending=0).head(5)
+df_general.sort_values(by=['EmployeeID'],ascending=1).head(5)
+df_manager.sort_values(by=['EmployeeID'],ascending=1).head(5)
+df_retirement.sort_values(by=['EmployeeID'],ascending=1).head(5)
 
 # Obtener información sobre los DataFrames
 df_employee.info(verbose=True)
@@ -43,34 +43,49 @@ df = df_employee.merge(df_general, on='EmployeeID', how='inner')\
 
 # Procedemos a revisar los valores nulos
 print(df.isnull().sum())
-df[df['TotalWorkingYears'].isnull()] 
+df[df['TotalWorkingYears'].isnull()]
+print(df.columns)
 
 ##### Aqui se solucionan los nulos de algunas columnas #####
 
 # Se cambia a cero los valores nulos de la columna 'NumCompaniesWorked' asumiendo que esta es su primera empresa y habiendo verificado que todos iniciaron siendo mayores de edad
+df[df['NumCompaniesWorked'].isnull()]
 df['NumCompaniesWorked'] = df['NumCompaniesWorked'].fillna(0)
 # Para la columna 'EnvironmentSatisfaction' se observó la mediana y el promedio y se llegó a la conclusión de que 3 es el valor más adecuado para no interferir de una
 # manera significativa en el resultado y por lo tanto los valores nulos se remplazarán por 3
 df['EnvironmentSatisfaction'].value_counts()
+df['EnvironmentSatisfaction'].mean()
+df['EnvironmentSatisfaction'].median()
+df[df['EnvironmentSatisfaction'].isnull()]
 df['EnvironmentSatisfaction'] = df['EnvironmentSatisfaction'].fillna(3)
 # Para la columna 'JobSatisfaction' se observo el promedio y un conteo de repeticion y se llego a la conclusion de que el 4 era el valor más adecuado para no interferir de una
-# manera significativa en el resultado y por lo tanto los valores nulos se remplazaran por 4
+# manera significativa en el resultado y por lo tanto los valores nulos se remplazaran por 3
 df['JobSatisfaction'].value_counts()
-df['JobSatisfaction'] = df['JobSatisfaction'].fillna(4)
+df['JobSatisfaction'].mean()
+df['JobSatisfaction'].median()
+df[df['JobSatisfaction'].isnull()]
+df['JobSatisfaction'] = df['JobSatisfaction'].fillna(3)
 # Para la columna 'WorkLifeBalance' se observo el promedio y un conteo de repeticion y se llego a la conclusion de que el 3 era el valor maas adecuado para no interferir de una
 # manera significativa en el resultado y por lo tanto los valores nulos se remplazaran por 3
 df['WorkLifeBalance'].value_counts()
+df['WorkLifeBalance'].mean()
+df['WorkLifeBalance'].median()
+df[df['WorkLifeBalance'].isnull()]
 df['WorkLifeBalance'] = df['WorkLifeBalance'].fillna(3)
 # Remplazar los valores de los años trabajados en la empresa en la columan de años totales trabajdos para quitar los nulos
+df['TotalWorkingYears'].isnull().sum()
 df['TotalWorkingYears'].fillna(df['YearsAtCompany'], inplace=True)
 
 #Al volver a revisar vemos que las columnas retirementDate, retirementType, y resignationReason tienen valores nulos, lo cual es esperado dado que no todos los empleados se habrán retirado o renunciado.
 print(df.isnull().sum())
 
 #La columna retirementDate contiene fechas en formato de cadena de texto (string), que deberían convertirse a un formato de fecha.
+df['retirementDate'].info()
 df['retirementDate'] = pd.to_datetime(df['retirementDate'], dayfirst=True)
 
 #Las columnas retirementType y resignationReason contienen valores categóricos en formato de cadena de texto.
+print(df['retirementType'].dtype)
+print(df['resignationReason'].dtype)
 df['retirementType'] = df['retirementType'].astype('category')
 df['resignationReason'] = df['resignationReason'].astype('category')
 
@@ -85,7 +100,7 @@ df.drop(columns=["EmployeeCount", "Over18", "StandardHours"],inplace=True)
 conn = sql.connect("db_empleados")  # creacion de la base de datos
 cursor = conn.cursor() # para funcion execute
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-cursor.fetchall() #para ver en tabalas las bases de datos
+cursor.fetchall() # para ver en tablas las bases de datos
 
 # Llevar DataFrame a la base de datos
 df.to_sql("all_employees", conn, if_exists="replace")
@@ -120,7 +135,8 @@ pd.read_sql("""SELECT YearsAtCompany, COUNT(*) as Retirements
 pd.read_sql("""SELECT Age, COUNT(*) as Retirements 
                                     FROM all_employees 
                                     WHERE retirementDate IS NOT NULL 
-                                    GROUP BY Age""", conn)
+                                    GROUP BY Age
+                                    ORDER BY Retirements DESC""", conn)
 
 #Razones de retiro
 pd.read_sql("""SELECT resignationReason, COUNT(*) as Count 
